3009bbabf796a2f4a499367728df8dbd49f7a557
Andre Pierre <andre.laurence94@gmail.com>
2020-08-15 06:14:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sent)
   # word = sent.split(" ") 
   reverse = [] 
   
-  sent.collect {sent.collect.reverse! {|rev| reverse << rev}}
+  sent.collect {|rev| reverse << rev.reverse!}}
   
   return reverse.join(" ")
 end
